2a3a1bbed52705dc65322589750d67f57ef391a1
Skier23 <ski23@vt.edu>
2017-07-12 11:31:43 -0400

    o

--- register/register.py
+++ register/register.py
@@ -40,6 +40,32 @@ class Register:
             data = discord.Embed(colour=user.colour)
             data.add_field(name="Error:warning:",value="Oops, it seems like you already have an account, {}.".format(user.mention))
             await self.bot.say(embed=data)
+	@commands.command(name="remove", pass_context=True, invoke_without_command=True, no_pm=True)
+    async def _rem(self, ctx, rockstarID : str):
+        """Sign up with your SocialClub name to join channels and drops"""
+		
+		
+        server = ctx.message.server
+        user = ctx.message.author
+        
+        if server.id not in self.usersArray:
+            self.usersArray[server.id] = {}
+        if user.id not in self.usersArray[server.id]:
+			
+			#for thisUser in self.usersArray[server.id]
+			#	if self.usersArray[server.id][user.id]["SocialClub"] == rockstarID
+			#		await self.bot.say("A user has already registered with this SocialClub. Try again.")
+			#		return
+            self.usersArray[server.id][user.id] = {}
+			#self.usersArray[server.id][user.id].update({"SocialClub" : rockstarID})
+            dataIO.save_json(self.profile, self.usersArray)
+            data = discord.Embed(colour=user.colour)
+            data.add_field(name="Congrats!:sparkles:", value="You have officially created your account for **{}**, {}.".format(server, user.mention))
+            await self.bot.say(embed=data)
+        else: 
+            data = discord.Embed(colour=user.colour)
+            data.add_field(name="Error:warning:",value="Oops, it seems like you already have an account, {}.".format(user.mention))
+            await self.bot.say(embed=data)
     @commands.command(name="account", pass_context=True, invoke_without_command=True, no_pm=True)
     async def _acc(self, ctx, user : discord.Member=None):
         """Your/Others Register"""
