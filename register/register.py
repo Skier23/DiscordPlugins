ea4e3ecf1169983cd870daddcdb007aa7f634dbc
Skier23 <ski23@vt.edu>
2017-07-12 10:45:47 -0400

    tt

--- register/register.py
+++ register/register.py
@@ -55,7 +55,7 @@ class Register:
         if user.id not in self.usersArray[server.id]:
             await self.bot.say("User is not registered")
         else: 
-            self.usersArray[server.id][user.id] = {}
+            #self.usersArray[server.id][user.id] = {}
 			#self.usersArray[server.id].remove(user.id) 
 			dataIO.save_json(self.profile, self.usersArray)
             await self.bot.say(embed=data)
