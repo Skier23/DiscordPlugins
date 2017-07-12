3d953feb21ddc80e55370bee0f9f5cc4e8f9586f
Skier23 <ski23@vt.edu>
2017-07-12 15:34:04 -0400

    tt

--- register/register.py
+++ register/register.py
@@ -27,10 +27,10 @@ class Register:
         if user.id not in self.usersArray[server.id]:
             for key in self.usersArray[server.id]:
                 if "SocialClub" in self.usersArray[server.id][user.id]:
-                    await self.bot.say("flag1")
-                    if self.usersArray[server.id][user.id]["SocialClub"] == rockstarID:
-                        await self.bot.say("A user has already registered with this SocialClub. Try again.")
-                        #return
+                    #await self.bot.say("flag1")
+                    #if self.usersArray[server.id][user.id]["SocialClub"] == rockstarID:
+                    #    await self.bot.say("A user has already registered with this SocialClub. Try again.")
+                    #    return
             self.usersArray[server.id][user.id] = {}
             #self.usersArray[server.id][user.id].update({"SocialClub" : rockstarID})
             dataIO.save_json(self.profile, self.usersArray)
