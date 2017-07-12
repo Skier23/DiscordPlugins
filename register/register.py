edc42dbd3e831dc2d115aad924bf51459c5d9795
Skier23 <ski23@vt.edu>
2017-07-12 10:37:01 -0400

    adding del

--- register/register.py
+++ register/register.py
@@ -42,6 +42,25 @@ class Register:
             await self.bot.say(embed=data)
         
     
+	@commands.command(name="remove", pass_context=True, invoke_without_command=True, no_pm=True)
+    async def _rem(self, ctx, user : discord.Member=None):
+        """Remove the specified member from the registry"""
+		
+		
+        server = ctx.message.server
+		if user == None			
+			user = ctx.message.author
+        if server.id not in self.usersArray:
+            self.usersArray[server.id] = {}
+        if user.id not in self.usersArray[server.id]:
+            await self.bot.say("User is not registered")
+        else: 
+            self.usersArray[server.id][user.id] = {}
+			self.usersArray[server.id].remove(user.id)
+			dataIO.save_json(self.profile, self.usersArray)
+            await self.bot.say(embed=data)
+        
+    
     @commands.command(name="account", pass_context=True, invoke_without_command=True, no_pm=True)
     async def _acc(self, ctx, user : discord.Member=None):
         """Your/Others Register"""
