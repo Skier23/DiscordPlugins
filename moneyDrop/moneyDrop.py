947d8c937a9145f84a9b784b03df2d3a4751cc4d
Skier23 <ski23@vt.edu>
2017-07-18 23:05:29 -0400

    il

--- moneyDrop/moneyDrop.py
+++ moneyDrop/moneyDrop.py
@@ -12,9 +12,9 @@ class moneyDrop:
 
     def __init__(self, bot):
         self.bot = bot    
-    async def _schedule_close(self, member, survey_id: str, delay: int):
+    def _schedule_close(self, member, survey_id: str, delay: int):
         new_handle = self.bot.loop.call_later(
-            delay, self._mark_as_closed, survey_id, member)
+            delay, await self._mark_as_closed, survey_id, member)
     @commands.command(name="startdrop", pass_context=True, invoke_without_command=True)
     async def startDrop(self, ctx, member: discord.Member):
         print("testinconsole")
