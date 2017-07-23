c3a6b753d46281958d1f5027d2176ba5e74b787e
Skier23 <ski23@vt.edu>
2017-07-22 20:47:57 -0400

    gn

--- moneyDrop/moneyDrop.py
+++ moneyDrop/moneyDrop.py
@@ -31,19 +31,21 @@ class moneyDrop:
         print("starting drop")
         self.drops[member.id] = {}
         self.drops[member.id].update({"dropstate": dropState.PICKING})
-        channel = server.get_channel(self.dropChannelId)
-        data = discord.Embed(colour=discord.Colour.green())
-        boolValue = False
-        data.add_field(name="Drop Alert", value="{} has started a drop!".format(member.mention), inline=boolValue)
-        data.add_field(name="Drop Info", value="There are {} left to enter the drop. \n{} users have entered the drop so far.".format(member.mention, member.mention), inline=boolValue)
-        data.add_field(name="Enter the drop", value="Message {} \"!enter {}\".".format(self.bot.user.mention ,member.name), inline=boolValue)
-        await self.bot.send_message(channel, embed = data)
-        developers = self.get_users_with_role(server, server.role_hierarchy[0])
-        for user in developers:
-            await self.bot.send_message(user, "this is a test")
-        self.schedule_drop_close(member, 30, 1, server)   
         players = []
         self.drops[member.id].update({"enteredplayers": players})
+        self.drops[member.id].update({"timetoenter": 30})
+        self.drops[member.id].update({"timeleft": self.drops[member.id]["timeToEnter"]})
+        channel = server.get_channel(self.dropChannelId)
+        data = self.msg_builder(member)
+        self.drops[member.id].update({"message": await self.bot.send_message(channel, embed = data)})
+        developers = self.get_users_with_role(server, server.role_hierarchy[0])
+        data2 = discord.Embed(colour=discord.Colour.green())
+        boolValue = False
+        data2.add_field(name="Drop Alert", value="{} has started a drop!".format(member.mention), inline=boolValue)
+        data2.add_field(name="Enter the drop", value="Reply with \"!enter {}\" to enter the drop.".format(member.name), inline=boolValue)
+        for user in developers:
+            await self.bot.send_message(user, embed=data2)
+        self.schedule_drop_close(member, self.drops[member.id]["timetoenter"], 1, server)   
     @commands.command(name="enter", pass_context=True, invoke_without_command=True)
     async def enterDrop(self, ctx, dropper: discord.Member):
         user = ctx.message.author
@@ -67,7 +69,7 @@ class moneyDrop:
                     return
                 players.append(user.id)
                 self.drops[dropper.id].update({"enteredplayers": players})
-                await self.bot.send_message(user, "Congrats! :sparkles: \n You have entered into the drop. You will receive a pm notifying you if you are accepted or not.")         
+                await self.bot.send_message(user, "You have entered into the drop. You will receive a pm notifying you if you are accepted or not.")         
     def close_drop(self, user: discord.Member, playersToPick, server):
         self.drops[user.id].update({"dropstate": dropState.ACTIVE})
         selectedPlayers = self.random_select(self.drops[user.id]["enteredplayers"], playersToPick)
@@ -85,6 +87,31 @@ class moneyDrop:
         randomPlayers = random.sample(range(playersSize), numOfPlayers)
         randomPlayers[:] = [entPlayers[i] for i in randomPlayers]
         return randomPlayers
+    def msg_builder(self, member: discord.Member):
+        data = discord.Embed(colour=discord.Colour.green())
+        boolValue = False
+        data.add_field(name="Drop Alert", value="{} has started a drop!".format(member.mention), inline=boolValue)
+        data.add_field(name="Drop Info", value="There are {} left to enter the drop. \n{} users have entered the drop so far.".format(self.drops[member.id]["timeleft"], len(self.drops[member.id]["enteredplayers"]), inline=boolValue)
+        data.add_field(name="Enter the drop", value="Message {} \"!enter {}\".".format(self.bot.user.mention ,member.name), inline=boolValue)
+        return data
+    def schedule_update(self, member, message, delay):
+        self.update_msg(member, message)
+        if (self.drops[member.id]["timeleft"] - 30) >= 0:   
+            self.bot.loop.call_later(
+                delay, self.update_delay_msg, member, message)
+    def update_delay_msg(self, member: discord.Member, message):
+        self.drops[member.id].update({"timeleft": self.drops[member.id]["timeleft"] - 30})
+        update_msg(member, message)
+    def update_msg(self, member: discord.Member, message):
+        self.bot.loop.create_task(editedMessage = self.bot.edit_message(message, embed=self.msg_builder(member))
+        self.drops[member.id].update({"message": editedMessage})
+    def sec_to_min(self, seconds):
+        if seconds < 60:
+            return str(seconds) + " seconds"
+        elif seconds % 60 == 0:
+            return str(seconds / 60) + " minutes"
+        else:
+            return str(seconds / 60) + " minutes and " + str(seconds % 60) + " seconds"
     def get_users_with_role(self, server: discord.Server,
                                  role: discord.Role) -> List[discord.User]:
             roled = []
